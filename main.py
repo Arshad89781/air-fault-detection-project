51fc3bba387c43086d4955e2eb9c80e6ab620668
Arshad89781 <git config --global user.email you@example.com>
2022-12-23 00:10:05 +0530

    updated data ingestion pipeline

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import pymongo
+# from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.components import data_ingestion
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Database Name
-dataBase = client["neurolabDB"]
 
-# Collection  Name
-collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+if __name__ == "__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+          # get_collection_as_dataframe(database_name='aps', collection_name='sensor')
+     except Exception as e:
+          print(e)
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
+# # Provide the mongodb localhost url to connect python to mongodb.
+# client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
+# # Database Name
+# dataBase = client["neurolabDB"]
 
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+# # Collection  Name
+# collection = dataBase['Products']
+
+# # Sample data
+# d = {'companyName': 'iNeuron',
+#      'product': 'Affordable AI',
+#      'courseOffered': 'Machine Learning with Deployment'}
+
+# # Insert above records in the collection
+# rec = collection.insert_one(d)
+
+# # Lets Verify all the record at once present in the record with all the fields
+# all_record = collection.find()
+
+# # Printing all records present in the collection
+# for idx, record in enumerate(all_record):
+#      print(f"{idx}: {record}")
