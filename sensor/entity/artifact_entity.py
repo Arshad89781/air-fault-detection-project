a889ebb3c0f34996a18a16da2b38c7bf2ef84958
Arshad89781 <syedarshadalisha9@gmail.com>
2022-12-26 20:43:38 +0530

    updated data transformation and data training pipeline

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -10,7 +10,18 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     report_file_path:str
     
-class DataTransformationArtifact:...
-class ModelTrainerArtifact:...
+@dataclass
+class DataTransformationArtifact:
+    transform_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    target_encoder_path:str
+    
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
+    
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
